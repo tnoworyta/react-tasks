90f9012831d3badf385fc3e05b2f94075d50b3af
Tomasz Noworyta <tomasz.noworyta@gmail.com>
2020-02-07 13:46:47 +0100

    Updated user list

--- src/components/User/List.js
+++ src/components/User/List.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
 
-import UserNew from './New'
-import UserDetails from './Details'
+import UserNew from "./New";
+import UserDetails from "./Details";
 
 export default class UserList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       users: [
-        { name: 'Example name', surname: 'Example surname', id: new Date().getTime().toString() },
-      ],
-    }
+        {
+          name: "Example name",
+          surname: "Example surname",
+          id: new Date().getTime().toString()
+        }
+      ]
+    };
   }
 
-  handleAddUserClick = user => {
-    this.setState(state => ({
+  handleAddUserClick = (user) => {
+    this.setState((state) => ({
       users: state.users.concat({
         name: user.name,
         surname: user.surname,
-        id: new Date().getTime().toString(),
-      }),
-    }))
-  }
+        id: new Date().getTime().toString()
+      })
+    }));
+  };
 
   render() {
-    const { users } = this.state
+    const {users} = this.state;
     return (
       <div>
         <UserNew handleAddUserClick={this.handleAddUserClick} />
-        <h3>User list</h3>
+        <h3>User list Updated</h3>
         <ul>
-          {users.map(user => (
+          {users.map((user) => (
             <li key={user.id}>
               <UserDetails user={user} />
             </li>
           ))}
         </ul>
       </div>
-    )
+    );
   }
 }
