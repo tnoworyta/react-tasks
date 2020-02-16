0f0e1f6d7159444fbb300c7714ce05325c120add
Tomasz Noworyta <tomasz.noworyta@gmail.com>
2020-02-16 13:23:11 +0100

    Add tests for timers and date

--- src/components/Product/List.js
+++ src/components/Product/List.js
@@ -1,30 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ProductDetails from './Details'
+import ProductDetails from './Details';
 
 export default class ProductList extends Component {
   state = {
-    productLikes: {}
-  }
+    productLikes: {},
+    showNotice: false,
+  };
 
   onLikeProduct = (product) =>
-    this.setState((state) =>
-      ({ productLikes: { ...state.productLikes, [product.id]: (state.productLikes[product.id] || 0) + 1 } }))
+    this.setState(
+      (state) => ({
+        showNotice: true,
+        productLikes: {
+          ...state.productLikes,
+          [product.id]: (state.productLikes[product.id] || 0) + 1,
+        },
+      }),
+      () =>
+        setTimeout(() => {
+          this.setState({ showNotice: false });
+        }, 2000),
+    );
+
+  currentDate = () => {
+    const date = new Date();
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+    return `${day}-${monthIndex + 1}-${year}`;
+  };
 
   render() {
-    const { products } = this.props
-    const { productLikes } = this.state
+    const { products } = this.props;
+    const { showNotice, productLikes } = this.state;
     return (
       <div>
-        {products.map(product =>
+        <div className="notice">{showNotice ? 'Product liked!' : 'Waiting for likes'}</div>
+        <div className="status">State for {this.currentDate()}</div>
+        {products.map((product) => (
           <ProductDetails
-           key={product.id}
-           product={product}
-           onLike={this.onLikeProduct}
-           likes={productLikes[product.id] || 0}
+            key={product.id}
+            product={product}
+            onLike={this.onLikeProduct}
+            likes={productLikes[product.id] || 0}
           />
-        )}
+        ))}
       </div>
-    )
+    );
   }
 }
--- src/components/Product/__tests__/List.test.js
+++ src/components/Product/__tests__/List.test.js
@@ -1,14 +1,58 @@
-import React from 'react'
-import { mount } from 'enzyme'
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import MockDate from 'mockdate';
 
-import List from '../List'
+import List from '../List';
 
 describe('<List />', () => {
-  test('simulates like clicked', () => {
-    const props = { products: [{ id: '1', name: 'Milk', price: 10.99 }, { id: '2', name: 'Bread', price: 9.99 }] }
-    const wrapper = mount(<List {...props} />)
+  it('simulates like clicked', () => {
+    const props = {
+      products: [
+        { id: '1', name: 'Milk', price: 10.99 },
+        { id: '2', name: 'Bread', price: 9.99 },
+      ],
+    };
+    const wrapper = mount(<List {...props} />);
 
-    wrapper.find('button').first().simulate('click');
-    expect(wrapper.find('.like-count').first().text()).toEqual('1 likes');
-  })
-})
+    wrapper
+      .find('button')
+      .first()
+      .simulate('click');
+    expect(
+      wrapper
+        .find('.like-count')
+        .first()
+        .text(),
+    ).toEqual('1 likes');
+  });
+
+  it('should show notice when liked for 2 seconds', () => {
+    jest.useFakeTimers();
+    const props = {
+      products: [
+        { id: '1', name: 'Milk', price: 10.99 },
+        { id: '2', name: 'Bread', price: 9.99 },
+      ],
+    };
+    const wrapper = mount(<List {...props} />);
+
+    wrapper
+      .find('button')
+      .first()
+      .simulate('click');
+
+    expect(wrapper.find('.notice').text()).toEqual('Product liked!');
+    jest.advanceTimersByTime(2000);
+    expect(wrapper.find('.notice').text()).toEqual('Waiting for likes');
+  });
+
+  it('should show current date', () => {
+    MockDate.set('1/15/2020');
+    const props = {
+      products: [],
+    };
+    const wrapper = shallow(<List {...props} />);
+
+    expect(wrapper.find('.status').text()).toEqual('State for 15-1-2020');
+  });
+});
